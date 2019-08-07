8ced55bcd160363576173f04c39ebfdd51a73f33
Rishabh <f20170111@goa.bits-pilani.ac.in>
2019-08-07 07:54:05 +0530

    init

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get('/', (req, res) => res.send('This is the backend for my regular apps'));
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
